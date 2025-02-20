5ba2eb60d962c0ad90e47dcec0717a1ba497c65f
militaovitor01 <vdiasmilitao@gmail.com>
2025-02-19 22:33:15 -0300

    Add rota favoritos

--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import Input from "../Input";
-import { useState } from "react";
-import { livros } from "./dadosPesquisa";
+import { useEffect, useState } from "react";
+import { getLivros } from "../servicos/livros";
+
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -47,7 +48,16 @@ const ResultadoPesquisa = styled.div`
 
 function Pesquisa(){
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
-    console.log(livrosPesquisados)
+    const [livros, setLivros] = useState([])
+
+    useEffect(() =>{
+        fetchLivros()
+    }, [])
+
+    async function fetchLivros() {
+        const livrosAPI = await getLivros()
+        setLivros(livrosAPI)
+    }
 
     return(
         <PesquisaContainer>
--- src/components/rotas/Favoritos.js
+++ src/components/rotas/Favoritos.js
@@ -1,18 +1,76 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-import Pesquisa from "../Pesquisa";
-import UltimosLancamentos from "../UltimosLancamentos";
+import { getFavoritos } from "../servicos/favoritos";
+import livroImg from '../../images/livro.png'
 
-const AppContainer = styled.div` 
+const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: linear-gradient(90deg, #002F52 35%, #326589);
-`
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+`;
+
+const ResultadoContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+  cursor: pointer;
+  text-align: center;
+  padding: 0 100px;
+  p {
+    width: 200px;
+    color: #fff;
+  }
+  img {
+    width: 100px;
+  }
+  &:hover {
+    border: 1px solid white;
+  }
+`;
+
+const Titulo = styled.h2`
+  color: #fff;
+  font-size: 36px;
+  text-align: center;
+  width: 100%;
+  padding-top: 35px;
+`;
 
 function Favoritos() {
+  const [favoritos, setFavoritos] = useState([]);
+
+  async function fetchFavoritos() {
+    const favoritosAPI = await getFavoritos();
+    setFavoritos(favoritosAPI);
+  }
+
+  useEffect(() => {
+    fetchFavoritos();
+  }, []);
+
   return (
     <AppContainer>
-      <Pesquisa/>
-    </AppContainer>    
+      <div>
+        <Titulo>Aqui estão os seus livros favoritos!</Titulo>
+        <ResultadoContainer>
+          {favoritos.length !== 0
+            ? favoritos.map((favorito) => (
+                <Resultado>
+                  <p>{favorito.nome}</p>
+                  <img src={livroImg} />
+                </Resultado>
+              ))
+            : null}
+        </ResultadoContainer>
+      </div>
+    </AppContainer>
   );
 }
 
